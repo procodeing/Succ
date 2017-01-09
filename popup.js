e6cbad2901928335d5e60cf51fa2a7ff0cfbba55
Indy Prieto <corepdesign@gmail.com>
2017-01-09 12:55:13 -0600

    Added base files

---
+++ popup.js
@@ -0,0 +1,31 @@
+function walk(node) {
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
+    var child, next;
+    switch (node.nodeType) {
+        case 1:
+            // Element
+        case 9:
+            // Document
+        case 11:
+            // Document fragment
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+        case 3:
+            // Text node
+            handleText(node);
+            break;
+    }
+}
+function handleText(textNode) {
+    var v = textNode.nodeValue;
+        
+        v = v.replace(RegExp(/\Bck/, "g"), "cc");
+        textNode.nodeValue = v;
+}
+walk(document.body)
